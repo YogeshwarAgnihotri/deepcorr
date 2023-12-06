102a9322c1810b7027b9b834da05f1be83701bbe
YogeshwarAgnihotri <y.agnihotri98@gmail.com>
2023-12-06 20:01:38 +0100

    Additions to train_test_split.py
    - Added path logic for having a run folder where the indexes of the
    test data are exported (and everything else related to a run)
    - Train ratio moved to main.py

--- code/train_test_split.py
+++ code/train_test_split.py
@@ -1,16 +1,30 @@
 import pickle
 import numpy as np
+from utlis import check_create_path, check_path_throw_error
+import os
 
-def split_train_test(dataset):
-    len_tr=len(dataset)
-    print("Dataset length: ", len_tr)
-    #train_ratio=float(len_tr-6000)/float(len_tr)
-    train_ratio=0.8
-    rr= range(len(dataset))
+
+
+def split_train_test(dataset, train_ratio, run_folder_path):
+    len_tr = len(dataset)
+    print 'Dataset length: ', len_tr
+    rr = range(len(dataset))
     np.random.shuffle(rr)
 
-    train_index=rr[:int(len_tr*train_ratio)]
-    test_index= rr[int(len_tr*train_ratio):] #range(len(dataset_test)) # #
-    pickle.dump(test_index,open('test_index300.pickle','w'))
+    train_index = rr[:int(len_tr*train_ratio)]
+    print 'Length of true correlating flow pairs for TRAINING: ', \
+          len(train_index), "flow pairs"
+
+    test_index = rr[int(len_tr*train_ratio):]
+    print "Length of true correlating flow pairs for TESTING: ", \
+          len(test_index), "flow pairs"
+
+
+    # Create path for testing indexes
+    test_indexes_path = os.path.join(run_folder_path, "testing_indexes.pickle")
+
+    # Save the testing indexes for later testing
+    with open(test_indexes_path, 'wb') as f:
+        pickle.dump(test_index, f)
 
     return train_index, test_index
