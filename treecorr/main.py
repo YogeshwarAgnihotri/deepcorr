248d9922128a1c5267bad47d5b4afc33cf9ce275
YogeshwarAgnihotri <y.agnihotri98@gmail.com>
2023-12-19 17:29:42 +0100

    Mulitple updates
    - Gridsearch for tree hyperparameters added
    - Possiblity of training individual parameterised trees added
    - cleanded up code

--- treecorr/main.py
+++ treecorr/main.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 import sys
 import os
+import yaml
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
@@ -10,6 +11,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support, confusion_matrix
+from sklearn.model_selection import GridSearchCV
 
 
 from shared.utils import create_path
@@ -19,25 +21,7 @@ from shared.train_test_split import calc_train_test_indexes
 
 import time
 
-#################### Parameters ####################
-# Set up argument parser
-parser = argparse.ArgumentParser(description='Train a Decision Tree Classifier on DeepCorr dataset.')
-parser.add_argument('--dataset_path', type=str, default="/home/yagnihotri/datasets/deepcorr_original_dataset", help='Path to the DeepCorr dataset')
-parser.add_argument('--train_ratio', type=float, default=0.8, help='Training set ratio')
-parser.add_argument('--flow_size', type=int, default=300, help='Flow size')
-parser.add_argument('--negative_samples', type=int, default=199, help='Number of negative samples')
-parser.add_argument('--load_all_data', action='store_true', help='If NOT set, only flows with minimum 300 packets will be loaded (about 7300 flow pairs). If set, all will be loaded (about 20-25k).')
-
-# Parse arguments
-args = parser.parse_args()
-
-# Extracting arguments
-dataset_path = args.dataset_path
-train_ratio = args.train_ratio
-flow_size = args.flow_size
-negative_samples = args.negative_samples
-load_all_data = args.load_all_data
-
+#################### Classes and Functions ####################
 class StreamToLogger:
     """
     Custom stream object that redirects writes to both a logger and the original stdout.
@@ -76,8 +60,174 @@ def setup_logger(name, log_file, level=logging.INFO):
 
     return logger
 
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
-#################### Paths ####################
+def format_time(seconds):
+    """Format time in seconds to days, hours, minutes, and seconds."""
+    days = seconds // (24 * 3600)
+    seconds %= (24 * 3600)
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return f"{int(days)}d {int(hours)}h {int(minutes)}m {seconds:.2f}s"
+
+def flatten_data_for_decision_tree(data):
+    """Flatten the data for feeding into a decision tree."""
+    return data.reshape(data.shape[0], -1)
+
+def evaluate_model_print_metrics(true_labels, predicted_labels):
+    # Evaluate the model
+    accuracy = accuracy_score(true_labels, predicted_labels)
+    precision, recall, f1, _ = precision_recall_fscore_support(true_labels, predicted_labels, average='binary')
+    conf_matrix = confusion_matrix(true_labels, predicted_labels)
+
+    # Print the evaluation results
+    print(f'\nAccuracy: {accuracy:.2f}')
+    print(f'Precision: {precision:.2f}')
+    print(f'Recall: {recall:.2f}')
+    print(f'F1 Score: {f1:.2f}')
+
+    # Assuming conf_matrix is the result of confusion_matrix(labels_test, y_pred)
+    TN, FP, FN, TP = conf_matrix.ravel()
+
+    print("Confusion Matrix:")
+    print(f"{'':>10} {'Predicted':>18}")
+    print(f"{'':>10} {'0':>8} {'1':>8}")
+    print(f"{'Actual 0':>10} {TN:>8} {FP:>8}")
+    print(f"{'Actual 1':>10} {FN:>8} {TP:>8}")
+
+    # Calculate True Positive Rate (TPR) also known as Recall
+    TPR = TP / (TP + FN)  # TPR = Recall
+    # Calculate False Positive Rate (FPR)
+    FPR = FP / (FP + TN)
+    print(f"True Positive Rate (TPR/Recall): {TPR:.2f}")
+    print(f"False Positive Rate (FPR): {FPR:.2f}")
+
+def dt_train_classifier_gridSearch(param_grid, cross_validation_folds, verbosity, training_data, labels):
+    grid_search = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=cross_validation_folds, scoring='accuracy', verbose=verbosity, n_jobs=-1)
+
+    # Start timing the training process
+    start_time_grid_search = time.time()
+
+    # do grid seach and make predictions with best model
+    print("\nTraining the model with grid search of tree hyperparameters...")
+    grid_search.fit(training_data, labels)
+    best_model = grid_search.best_estimator_
+    best_parameters = grid_search.best_params_
+    # End timing the training process with grid search
+    training_time_grid_search = time.time() - start_time_grid_search
+    print(f"Training completed in {format_time(training_time_grid_search)}")
+
+    print(f"Best parameters: {best_parameters}")
+
+    return best_model, best_parameters
+
+def dt_train(training_data, 
+             labels,
+             criterion="gini", 
+             splitter="best", 
+             max_depth=None, 
+             min_samples_split=2, 
+             min_samples_leaf=1, 
+             min_weight_fraction_leaf=0.0, 
+             max_features=None, 
+             random_state=None, 
+             max_leaf_nodes=None, 
+             min_impurity_decrease=0.0, 
+             class_weight=None, 
+             ccp_alpha=0.0):
+
+    # Create a Decision Tree Classifier
+    clf = DecisionTreeClassifier(criterion=criterion, 
+                                 splitter=splitter, 
+                                 max_depth=max_depth, 
+                                 min_samples_split=min_samples_split, 
+                                 min_samples_leaf=min_samples_leaf, 
+                                 min_weight_fraction_leaf=min_weight_fraction_leaf, 
+                                 max_features=max_features, 
+                                 random_state=random_state, 
+                                 max_leaf_nodes=max_leaf_nodes,
+                                 min_impurity_decrease=min_impurity_decrease, 
+                                 class_weight=class_weight, 
+                                 ccp_alpha=ccp_alpha)
+    # Start timing the training process
+    start_time_traning = time.time()
+    print("\nTraining the decision tree...")
+    # Train the model
+    model = clf.fit(training_data, labels)
+    # End timing the training process
+    training_time = time.time() - start_time_traning
+    print(f"Training of decision tree completed in {format_time(training_time)}")
+
+    return model
+
+#################### Parameters ####################
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Train a Decision Tree Classifier on DeepCorr dataset.')
+# data parameters
+parser.add_argument('--dataset_path', type=str, default="/home/yagnihotri/datasets/deepcorr_original_dataset", help='Path to the DeepCorr dataset')
+parser.add_argument('--train_ratio', type=float, default=0.8, help='Training set ratio')
+parser.add_argument('--flow_size', type=int, default=300, help='Flow size')
+parser.add_argument('--negative_samples', type=int, default=199, help='Number of negative samples')
+parser.add_argument('--load_all_data', action='store_true', help='If NOT set, only flows with minimum 300 packets will be loaded (about 7300 flow pairs). If set, all will be loaded (about 20-25k).')
+
+#### training parameters
+# Decision Tree parameters for a single tree to train
+parser.add_argument('--criterion', type=str, default="gini", help='The function to measure the quality of a split. Supported criteria are “gini” for the Gini impurity and “log_loss” and “entropy” both for the Shannon information gain')
+parser.add_argument('--splitter', type=str, default="best", help='The strategy used to choose the split at each node. Supported strategies are “best” to choose the best split and “random” to choose the best random split.')
+parser.add_argument('--max_depth', type=int, default=None, help='The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.')
+parser.add_argument('--min_samples_split', type=int, default=2, help='The minimum number of samples required to split an internal node')
+parser.add_argument('--min_samples_leaf', type=int, default=1, help='The minimum number of samples required to be at a leaf node')
+parser.add_argument('--min_weight_fraction_leaf', type=float, default=0.0, help='The minimum weighted fraction of the sum total of weights (of all the input samples) required to be at a leaf node. Samples have equal weight when sample_weight is not provided.')
+parser.add_argument('--max_features', type=str, default=None, help='The number of features to consider when looking for the best split')
+parser.add_argument('--random_state', type=int, default=None, help='Controls the randomness of the estimator. The features are always randomly permuted at each split, even if splitter is set to “best”. When max_features < n_features, the algorithm will select max_features at random at each split before finding the best split among them. But the best found split may vary across different runs, even if max_features=n_features. That is the case, if the improvement of the criterion is identical for several splits and one split has to be selected at random. To obtain a deterministic behaviour during fitting, random_state has to be fixed to an integer.')
+parser.add_argument('--max_leaf_nodes', type=int, default=None, help='Grow a tree with max_leaf_nodes in best-first fashion. Best nodes are defined as relative reduction in impurity. If None then unlimited number of leaf nodes.')
+parser.add_argument('--min_impurity_decrease', type=float, default=0.0, help='A node will be split if this split induces a decrease of the impurity greater than or equal to this value.')
+parser.add_argument('--class_weight', type=str, default=None, help='Weights associated with classes in the form {class_label: weight}. If not given, all classes are supposed to have weight one.')
+parser.add_argument('--ccp_alpha', type=float, default=0.0, help='Complexity parameter used for Minimal Cost-Complexity Pruning. The subtree with the largest cost complexity that is smaller than ccp_alpha will be chosen. By default, no pruning is performed.')
+
+#parameters to set for grid search of tree hyperparameters
+parser.add_argument('--do_tree_hyperparameter_search', action='store_true', help='If set, hyperparameter search will be done for the decision tree.')
+parser.add_argument('--param_grid_path', type=str, help='Path to JSON file containing parameter grid')
+parser.add_argument('--cross_validation_folds', type=int, default=5, help='Number of folds for cross validation')
+
+# Parse arguments
+args = parser.parse_args()
+
+# Extracting arguments
+dataset_path = args.dataset_path
+train_ratio = args.train_ratio
+flow_size = args.flow_size
+negative_samples = args.negative_samples
+load_all_data = args.load_all_data
+
+criterion = args.criterion
+splitter = args.splitter
+max_depth = args.max_depth
+min_samples_split = args.min_samples_split
+min_samples_leaf = args.min_samples_leaf
+min_weight_fraction_leaf = args.min_weight_fraction_leaf
+max_features = args.max_features
+random_state = args.random_state
+max_leaf_nodes = args.max_leaf_nodes
+min_impurity_decrease = args.min_impurity_decrease
+class_weight = args.class_weight
+ccp_alpha = args.ccp_alpha
+
+do_tree_hyperparameter_search = args.do_tree_hyperparameter_search
+param_grid_path = args.param_grid_path
+if do_tree_hyperparameter_search:
+    if param_grid_path is None:
+        raise ValueError("Please set the path to the JSON file containing the parameter grid for grid search.")
+    else:
+        param_grid = load_yaml(param_grid_path)
+
+cross_validation_folds = args.cross_validation_folds
+
+#################### Path Stuff ####################
 path_for_saving_run = "/home/yagnihotri/projects/corr/treecorr/runs"
 # TODO Change run_name to something shorter up sometime. maybe with config files that show the full parameters
 run_name = f"Date_{datetime.datetime.now().strftime('%d-%m-%Y_%H:%M:%S')}"
@@ -85,86 +235,47 @@ run_folder_path = os.path.join(path_for_saving_run, run_name)
 create_path(run_folder_path)
 
 output_file_path = os.path.join(run_folder_path, "training_log.txt")
-logger = setup_logger('DeepCorrTraining', output_file_path)
 
+logger = setup_logger('DeepCorrTraining', output_file_path)
 # Redirect stdout
 sys.stdout = StreamToLogger(logger, sys.stdout)
 
 # Loading deepcorr dataset
 deepcorr_dataset = load_dataset_deepcorr(dataset_path, load_all_data)
-
 # Split the dataset into training and test sets
 train_indexes, test_indexes = calc_train_test_indexes(deepcorr_dataset, train_ratio)
-
 # Preprocess the data and generate the data arrays for training and testing
 l2s, labels,l2s_test,labels_test = generate_flow_pairs(deepcorr_dataset, train_indexes, test_indexes, flow_size, run_folder_path, negative_samples)
 
-l2s_flattened = l2s.reshape(l2s.shape[0], -1)
+l2s_flattened = flatten_data_for_decision_tree(l2s)  # Flatten the data for feeding into a decision tree
 
-l2s_test_flattened = l2s_test.reshape(l2s_test.shape[0], -1)  # Same for test data
+l2s_test_flattened = flatten_data_for_decision_tree(l2s_test)  # Flatten the data for feeding into a decision tree
 
-# Create a Decision Tree Classifier
-clf = DecisionTreeClassifier()
+if do_tree_hyperparameter_search:
+    # do grid seach and make predictions with best model
+    best_model, best_parameters = dt_train_classifier_gridSearch(param_grid, cross_validation_folds, 3, l2s_flattened, labels)
+    predictions = best_model.predict(l2s_test_flattened)
 
-# Start timing the training process
-start_time = time.time()
-
-print("\nTraining the model...")
-# Train the model
-clf.fit(l2s_flattened, labels)
-
-# End timing the training process
-training_time = time.time() - start_time
-print(f'Training completed in {training_time:.2f} seconds')
-
-# Start timing the prediction process
-start_time = time.time()
-
-# Make predictions
-y_pred = clf.predict(l2s_test_flattened)
-
-# End timing the prediction process
-testing_time = time.time() - start_time
-print(f'Testing completed in {testing_time:.2f} seconds')
-
-# Evaluate the model
-accuracy = accuracy_score(labels_test, y_pred)
-precision, recall, f1, _ = precision_recall_fscore_support(labels_test, y_pred, average='binary')
-conf_matrix = confusion_matrix(labels_test, y_pred)
-
-# Print the evaluation results
-print(f'\nAccuracy: {accuracy:.2f}')
-print(f'Precision: {precision:.2f}')
-print(f'Recall: {recall:.2f}')
-print(f'F1 Score: {f1:.2f}')
-
-print('Confusion Matrix:')
-# Define column and row labels
-col_labels = ['Predicted Positive (0)', 'Predicted Negative (1)']
-row_labels = ['Actual Positive (0)', 'Actual Negative (1)']
-# Print column labels
-print(f"{'':>20} {' '.join(f'{label:>20}' for label in col_labels)}")
-# Print each row with its label
-for label, row in zip(row_labels, conf_matrix):
-    print(f'{label:>20} {" ".join(f"{num:20d}" for num in row)}')
-
-# True Positives (TP), False Positives (FP), True Negatives (TN), and False Negatives (FN)
-TP = conf_matrix[1, 1]
-FP = conf_matrix[0, 1]
-TN = conf_matrix[0, 0]
-FN = conf_matrix[1, 0]
-# Calculate True Positive Rate (TPR) also known as Recall
-TPR = TP / (TP + FN)  # TPR = Recall
-# Calculate False Positive Rate (FPR)
-FPR = FP / (FP + TN)
-print(f"True Positive Rate (TPR/Recall): {TPR:.2f}")
-print(f"False Positive Rate (FPR): {FPR:.2f}")
+else: # do normal training, with basic decision tree
+    model = dt_train(l2s_flattened, 
+                     labels, 
+                     criterion, 
+                     splitter, 
+                     max_depth, 
+                     min_samples_split, 
+                     min_samples_leaf, 
+                     min_weight_fraction_leaf, 
+                     max_features, 
+                     random_state, 
+                     max_leaf_nodes, 
+                     min_impurity_decrease, 
+                     class_weight, 
+                     ccp_alpha)
+    predictions = model.predict(l2s_test_flattened)
 
 
-# print parameters which were used for this run
-print("\nParameters:")
-print(f"dataset_path: {dataset_path}")
-print(f"train_ratio: {train_ratio}")
-print(f"flow_size: {flow_size}")
-print(f"negative_samples: {negative_samples}")
-print(f"load_all_data: {load_all_data}")
+evaluate_model_print_metrics(true_labels=labels_test, predicted_labels=predictions)
+
+#print all arguments set by the user for this run
+args_dict = vars(args)
+print(args_dict)
