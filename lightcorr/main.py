d97cf48d27abcc27f8f227c2036feba7266b814d
YogeshwarAgnihotri <y.agnihotri98@gmail.com>
2023-12-25 19:50:47 +0100

    Making the main and training modular and config usage
    Now it is easier import new models into this pipeline. the training now takes any model.
    
    Changed from cli to config arguments

--- lightcorr/main.py
+++ lightcorr/main.py
@@ -2,105 +2,87 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import argparse
-import datetime
+import yaml
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from shared.utils import create_path, StreamToLogger, setup_logger, load_yaml, create_run_folder
-from data_handling import load_data, prepare_data_for_dt_training, flatten_data_for_decision_tree
-from model_training import dt_train, dt_train_classifier_gridSearch, dt_train_classifier_randomSearch
+from data_handling import load_data, prepare_data_for_dt_training
+from model_training import train_model, train_classifier_gridSearch, train_classifier_randomSearch
 from evaluation import evaluate_model_print_metrics
-from arg_parser import parse_arguments
 
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
 def main():
-    #################### Parameters ####################
-    # Parse arguments
-    args = parse_arguments()
+    # Parse only the config file path
+    parser = argparse.ArgumentParser(description='Train a Classifier on the dataset.')
+    parser.add_argument('--config_path', type=str, required=True, help='Path to the configuration file')
+    args = parser.parse_args()
 
-    # Extracting arguments
-    dataset_path = args.dataset_path
-    run_folder_path = args.run_folder_path
-    train_ratio = args.train_ratio
-    flow_size = args.flow_size
-    negative_samples = args.negative_samples
-    load_all_data = args.load_all_data
+    # Load configuration
+    config = load_config(args.config_path)
 
-    criterion = args.criterion
-    splitter = args.splitter
-    max_depth = args.max_depth
-    min_samples_split = args.min_samples_split
-    min_samples_leaf = args.min_samples_leaf
-    min_weight_fraction_leaf = args.min_weight_fraction_leaf
-    max_features = args.max_features
-    random_state = args.random_state
-    max_leaf_nodes = args.max_leaf_nodes
-    min_impurity_decrease = args.min_impurity_decrease
-    class_weight = args.class_weight
-    ccp_alpha = args.ccp_alpha
+    # Extract settings from config
+    dataset_path = config['dataset_path']
+    run_folder_path = config['run_folder_path']
+    train_ratio = config['train_ratio']
+    flow_size = config['flow_size']
+    negative_samples = config['negative_samples']
+    load_all_data = config['load_all_data']
 
-    do_tree_grid_search = args.do_tree_grid_search
-    do_tree_random_search = args.do_tree_random_search
-    search_verbosity = args.search_verbosity
-    tree_param_path = args.tree_param_path
-
-    if do_tree_grid_search:
-        if tree_param_path is None:
-            raise ValueError("Please set the path to the JSON file containing the parameter grid for grid hyperparameter search.")
-        else:
-            param_grid = load_yaml(tree_param_path)
-
-    if do_tree_random_search:
-        if tree_param_path is None:
-            raise ValueError("Please set the path to the JSON file containing the parameter grid for random hyperparameter search.")
-        else:
-            param_grid = load_yaml(tree_param_path)
-
-    cross_validation_folds = args.cross_validation_folds
-    n_iter = args.n_iter
-
-    #################### Path Stuff ####################
+    # Prepare the run folder and logger
     run_folder_path = create_run_folder(run_folder_path)
     output_file_path = os.path.join(run_folder_path, "training_log.txt")
-    logger = setup_logger('DeepCorrTraining', output_file_path)
-    # Redirect stdout
+    logger = setup_logger('TrainingLogger', output_file_path)
     sys.stdout = StreamToLogger(logger, sys.stdout)
 
-    # Loading deepcorr dataset
+    # Load dataset
     deepcorr_dataset = load_data(dataset_path, load_all_data)
+    (flow_pairs_train, labels_train), (flow_pairs_test, labels_test) = prepare_data_for_dt_training(
+        deepcorr_dataset, train_ratio, flow_size, run_folder_path, negative_samples
+    )
 
-    (flow_pairs_train, labels_train), (flow_pairs_test, labels_test) = prepare_data_for_dt_training(deepcorr_dataset, train_ratio, flow_size, run_folder_path, negative_samples)
+    # Model initialization
+    model_type = config['model_type']
+    if model_type == 'decision_tree':
+        model = DecisionTreeClassifier(**config['decision_tree'])
+    elif model_type == 'random_forest':
+        model = RandomForestClassifier(**config['random_forest'])
+    else:
+        raise ValueError(f"Unsupported model type: {model_type}")
 
-    if do_tree_grid_search:
-        # do grid seach and make predictions with best model
-        best_model, best_parameters = dt_train_classifier_gridSearch(flow_pairs_train, labels_train, param_grid, cross_validation_folds, verbosity=search_verbosity)
-        predictions = best_model.predict(flow_pairs_test)
+    # Hyperparameter search settings
+    hyperparameter_search_config = config['hyperparameter_search']
+    search_type = hyperparameter_search_config['type']
 
-    if do_tree_random_search:
-        # do random seach and make predictions with best model
-        best_model, best_parameters = dt_train_classifier_randomSearch(flow_pairs_train, labels_train, param_grid, n_iter, cross_validation_folds, verbosity=search_verbosity)
-        predictions = best_model.predict(flow_pairs_test)
+    if search_type != 'none':
+        parameter_grid_path = hyperparameter_search_config['param_grid_path']
+        if parameter_grid_path:
+            parameter_grid = load_yaml(parameter_grid_path)
+        else:
+            raise ValueError("Parameter grid path must be specified for hyperparameter search.")
 
-    else: # do normal training, with basic decision tree
-        model = dt_train(flow_pairs_train, 
-                        labels_train, 
-                        criterion, 
-                        splitter, 
-                        max_depth, 
-                        min_samples_split, 
-                        min_samples_leaf, 
-                        min_weight_fraction_leaf, 
-                        max_features, 
-                        random_state, 
-                        max_leaf_nodes, 
-                        min_impurity_decrease, 
-                        class_weight, 
-                        ccp_alpha)
-        predictions = model.predict(flow_pairs_test)
+        if search_type == 'grid_search':
+            model, _ = train_classifier_gridSearch(
+                model, flow_pairs_train, labels_train, parameter_grid, hyperparameter_search_config['cross_validation_folds'], 
+                hyperparameter_search_config['search_verbosity']
+            )
+        elif search_type == 'random_search':
+            model, _ = train_classifier_randomSearch(
+                model, flow_pairs_train, labels_train, parameter_grid, hyperparameter_search_config['random_search']['n_iter'], 
+                hyperparameter_search_config['cross_validation_folds'], verbosity=hyperparameter_search_config['search_verbosity']
+            )
+    else:
+        model = train_model(model, flow_pairs_train, labels_train)
 
-
+    # Make predictions and evaluate the model
+    predictions = model.predict(flow_pairs_test)
     evaluate_model_print_metrics(true_labels=labels_test, predicted_labels=predictions)
 
-    #print all arguments set by the user for this run
-    args_dict = vars(args)
-    print(args_dict)
+    # Print all configurations used for this run
+    print(f"Configuration used: {config}")
 
 if __name__ == "__main__":
     main()
--- lightcorr/model_training.py
+++ lightcorr/model_training.py
@@ -1,96 +1,44 @@
-# model_training.py
-# Functions for training the decision tree model.
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 import time
-import datetime
 from shared.utils import format_time
 
-def dt_train_classifier_gridSearch(training_data, labels, param_grid, cross_validation_folds, verbosity):
-    grid_search = GridSearchCV(DecisionTreeClassifier(), param_grid, cv=cross_validation_folds, scoring='accuracy', verbose=verbosity, n_jobs=-1)
+def train_classifier_gridSearch(model, training_data, labels, param_grid, cross_validation_folds, verbosity):
+    grid_search = GridSearchCV(model, param_grid, cv=cross_validation_folds, scoring='accuracy', verbose=verbosity, n_jobs=-1)
 
-    # Start timing the training process
+    print("\nTraining the model with grid search...")
     start_time_grid_search = time.time()
-
-    # do grid seach and make predictions with best model
-    print("\nTraining the model with grid search of tree hyperparameters...")
     grid_search.fit(training_data, labels)
+    training_time_grid_search = time.time() - start_time_grid_search
+
     best_model = grid_search.best_estimator_
     best_parameters = grid_search.best_params_
-    # End timing the training process with grid search
-    training_time_grid_search = time.time() - start_time_grid_search
-    print(f"Training completed in {format_time(training_time_grid_search)}")
 
+    print(f"Training completed in {format_time(training_time_grid_search)}")
     print(f"Best parameters: {best_parameters}")
 
     return best_model, best_parameters
 
-def dt_train_classifier_randomSearch(training_data, labels, param_distributions, n_iter=10, cross_validation_folds=5, scoring=None, n_jobs=-1, refit=True, cv=None, verbosity=2, pre_dispatch='2*n_jobs', random_state=None, error_score='raise', return_train_score=False):
-    random_search = RandomizedSearchCV(
-        DecisionTreeClassifier(),
-        param_distributions,
-        n_iter=n_iter,
-        scoring=scoring,
-        n_jobs=n_jobs,
-        refit=refit,
-        cv=cv or cross_validation_folds,  # Use 'cv' if provided, else default to 'cross_validation_folds'
-        verbose=verbosity,
-        pre_dispatch=pre_dispatch,
-        random_state=random_state,
-        error_score=error_score,
-        return_train_score=return_train_score
-    )
+def train_classifier_randomSearch(model, training_data, labels, param_distributions, n_iter=10, cross_validation_folds=5, scoring=None, n_jobs=-1, refit=True, cv=None, verbosity=2, pre_dispatch='2*n_jobs', random_state=None, error_score='raise', return_train_score=False):
+    random_search = RandomizedSearchCV(model, param_distributions, n_iter=n_iter, scoring=scoring, n_jobs=n_jobs, refit=refit, cv=cv or cross_validation_folds, verbose=verbosity, pre_dispatch=pre_dispatch, random_state=random_state, error_score=error_score, return_train_score=return_train_score)
 
-    # Start timing the training process
+    print("\nTraining the model with random search...")
     start_time_random_search = time.time()
-
-    # Perform random search and fit the model
-    print("\nTraining the model with random search of tree hyperparameters...")
     random_search.fit(training_data, labels)
+    training_time_random_search = time.time() - start_time_random_search
+
     best_model = random_search.best_estimator_
     best_parameters = random_search.best_params_
 
-    # End timing the training process with random search
-    training_time_random_search = time.time() - start_time_random_search
     print(f"Training completed in {format_time(training_time_random_search)}")
     print(f"Best parameters: {best_parameters}")
 
     return best_model, best_parameters
-def dt_train(training_data, 
-             labels,
-             criterion="gini", 
-             splitter="best", 
-             max_depth=None, 
-             min_samples_split=2, 
-             min_samples_leaf=1, 
-             min_weight_fraction_leaf=0.0, 
-             max_features=None, 
-             random_state=None, 
-             max_leaf_nodes=None, 
-             min_impurity_decrease=0.0, 
-             class_weight=None, 
-             ccp_alpha=0.0):
 
-    # Create a Decision Tree Classifier
-    clf = DecisionTreeClassifier(criterion=criterion, 
-                                 splitter=splitter, 
-                                 max_depth=max_depth, 
-                                 min_samples_split=min_samples_split, 
-                                 min_samples_leaf=min_samples_leaf, 
-                                 min_weight_fraction_leaf=min_weight_fraction_leaf, 
-                                 max_features=max_features, 
-                                 random_state=random_state, 
-                                 max_leaf_nodes=max_leaf_nodes,
-                                 min_impurity_decrease=min_impurity_decrease, 
-                                 class_weight=class_weight, 
-                                 ccp_alpha=ccp_alpha)
-    # Start timing the training process
-    start_time_traning = time.time()
-    print("\nTraining the decision tree...")
-    # Train the model
-    model = clf.fit(training_data, labels)
-    # End timing the training process
-    training_time = time.time() - start_time_traning
-    print(f"Training of decision tree completed in {format_time(training_time)}")
+def train_model(model, training_data, labels):
+    print("\nTraining the model...")
+    start_time_training = time.time()
+    model.fit(training_data, labels)
+    training_time = time.time() - start_time_training
 
+    print(f"Training completed in {format_time(training_time)}")
     return model
