7b6eefbd8bd6f3417cd1d9f410ca042ab5a0a938
YogeshwarAgnihotri <y.agnihotri98@gmail.com>
2024-01-08 04:27:27 +0100

    renaming for better generalization

--- lightcorr/data_handling.py
+++ lightcorr/data_handling.py
@@ -1,10 +1,13 @@
-def prepare_data_for_dt_training(flow_pairs_train, labels_train, flow_pairs_test, labels_test):
-    flow_pairs_train = flatten_data_for_decision_tree(flow_pairs_train)  # Flatten the data for feeding into a decision tree
-    flow_pairs_test = flatten_data_for_decision_tree(flow_pairs_test)  # Flatten the data for feeding into a decision tree
+def prepare_data_for_training(flow_pairs_train, labels_train, flow_pairs_test, labels_test):
+    flow_pairs_train = flatten_data(flow_pairs_train)  # Flatten the data for feeding into a decision tree
+    flow_pairs_test = flatten_data(flow_pairs_test)  # Flatten the data for feeding into a decision tree
+
+    labels_train = labels_train.flatten()  # Flatten the labels for feeding into a decision tree
+    labels_test = labels_test.flatten()  # Flatten the labels for feeding into a decision tree
     
     return flow_pairs_train, labels_train, flow_pairs_test, labels_test
 
-def flatten_data_for_decision_tree(data):
+def flatten_data(data):
     """
     Flatten the data so it can be fed into a decision tree.
 
