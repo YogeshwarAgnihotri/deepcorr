b96aa1c02ef6b6a628ab58586d66c098155e1068
YogeshwarAgnihotri <y.agnihotri98@gmail.com>
2024-01-16 18:17:35 +0100

    Manual Split Code

--- shared/train_test_split.py
+++ shared/train_test_split.py
@@ -2,8 +2,8 @@ import pickle
 import numpy as np
 import os
 
-def calc_train_test_indexes(dataset, train_ratio):
-    print("\nSplitting dataset into training and testing sets...")
+def calc_train_test_indexes_using_ratio(dataset, train_ratio):
+    print(f"\nSplitting dataset into training and testing sets using train_ratio {train_ratio}...")
     len_tr = len(dataset)
 
     rr = list(range(len(dataset)))
@@ -17,6 +17,26 @@ def calc_train_test_indexes(dataset, train_ratio):
     print("Dataset length/Amount of true flow pairs for TESTING: ", \
           len(test_indexes), "true flow pairs")
     
+    return train_indexes, test_indexes
+
+def calc_train_test_index_manual_split(dataset, training_true_flow_amount, testing_true_flow_amount):
+    print(f"\nSplitting dataset into training and testing sets using manual split. Training true flow amount {training_true_flow_amount}/Testing True Flow amount {testing_true_flow_amount}...")
+    len_tr = len(dataset)
+
+    rr = list(range(len_tr))
+    np.random.shuffle(rr)
+
+    # Ensure that the sum of train_limit and test_limit doesn't exceed the dataset size
+    if training_true_flow_amount + testing_true_flow_amount > len_tr:
+        raise ValueError("Combined training and testing limits exceed the dataset size.")
+
+    train_indexes = rr[:training_true_flow_amount]
+    print('Dataset length/Amount of true flow pairs for TRAINING: ', len(train_indexes), "true flow pairs")
+
+    # Select indices for the test set right after the training set indices
+    test_indexes = rr[training_true_flow_amount:training_true_flow_amount + testing_true_flow_amount]
+    print("Dataset length/Amount of true flow pairs for TESTING: ", len(test_indexes), "true flow pairs")
+
     return train_indexes, test_indexes
 
 def save_test_indexes_to_path(test_indexes, path):
