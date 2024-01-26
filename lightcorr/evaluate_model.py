f56b935787d9f38aeca62aa0ffd03ac9485b17a3
Yogeshwar Agnihotri <y.agnihotri98@gmail.com>
2024-01-26 22:43:48 +0000

    model evaluation completed
    with roc curve added

--- lightcorr/evaluate_model.py
+++ lightcorr/evaluate_model.py
@@ -1,13 +1,33 @@
-from lightcorr.model_validation import evaluate_test_set
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import joblib
+
+from shared.model_evaluation import evaluate_test_set
+from shared.data_handling import load_pregenerated_memmap_dataset
+from shared.data_processing import flatten_generated_flow_pairs, flatten_arrays
+from shared.utils import create_run_folder
 
 def main():
+    # parameter
+    model_path = "/home/yogeshwar/master_thesis_corr/lightcorr/runs/26-01-2024_20:47:35_debug/model.joblib"
+    pregenerated_dataset_path = "/home/yogeshwar/datasets/deepcorr_pregen/deepcorr_9008_2252"
+    run_folder_path = "/home/yogeshwar/master_thesis_corr/lightcorr"
+    run_name = "test_eval"
+
+    run_folder_path = create_run_folder(run_folder_path, run_name)
+
+    model = joblib.load(model_path)
+
+    flow_pairs_train, labels_train, flow_pairs_test, labels_test = load_pregenerated_memmap_dataset(pregenerated_dataset_path)
+    
+    flattend_flow_pairs_test = flatten_generated_flow_pairs(flow_pairs_test)[0]
+
+    flattend_labels_test = flatten_arrays(labels_test)[0]
+
     # Evaluate the model on the test set
-    evaluate_test_set(model, flattend_flow_pairs_test, flattend_labels_test)
+    evaluate_test_set(model, flattend_flow_pairs_test, flattend_labels_test, run_folder_path)
 
 if __name__ == "__main__":
     main()
-
-# ######################################Evaluation Settings######################################
-# evaluation_settings:
-#   roc_plot_enabled: false  # If true, generate ROC plot   # TODO IMPLEMENT roc with test
-#   evaluate_on_test_set: false  # If true, evaluate the model on the test set
--- shared/model_evaluation.py
+++ shared/model_evaluation.py
@@ -1,21 +1,35 @@
-from sklearn.base import accuracy_score
-from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+import os
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.metrics import RocCurveDisplay, auc, confusion_matrix, precision_recall_fscore_support, roc_curve,accuracy_score
+
+from shared.utils import save_plot_to_path, save_array_to_file
 
 
-def evaluate_test_set(model, training_data, labels_test):
+def evaluate_test_set(model, training_data, labels_test, run_folder_path):
     print("\nEvaluating the model on the test set...")
-    # # Make predictions and evaluate the model
-    predictions = model.predict(training_data)
-    evaluate_model_print_metrics(true_labels=labels_test, predicted_labels=predictions)
+    # Make predictions using predict_proba to get the probability scores
+    scores = model.predict_proba(training_data)[:, 1]  # assuming the second column is for the positive class
+    evaluate_model_print_metrics(true_labels=labels_test, predicted_labels=(scores > 0.5).astype(int))
+    fig_linear, fig_roc = calc_roc_curves(true_labels=labels_test, 
+                                          predicted_scores=scores,
+                                          run_folder_path=run_folder_path)
+
+
+    # Save the ROC curves to the run folder
+    save_plot_to_path(fig_linear, "eval_roc_curve_linear.png", run_folder_path)
+    save_plot_to_path(fig_roc, "eval_roc_curve_log.png", run_folder_path)
+
     
 def evaluate_model_print_metrics(true_labels, predicted_labels):
+    print("\nMetrics for threshold = 0.5")
     # Evaluate the model
     accuracy = accuracy_score(true_labels, predicted_labels)
     precision, recall, f1, _ = precision_recall_fscore_support(true_labels, predicted_labels, average='binary')
     conf_matrix = confusion_matrix(true_labels, predicted_labels)
 
     # Print the evaluation results
-    print(f'\nAccuracy: {accuracy:.2f}')
+    print(f'Accuracy: {accuracy:.2f}')
     print(f'Precision: {precision:.2f}')
     print(f'Recall: {recall:.2f}')
     print(f'F1 Score: {f1:.2f}')
@@ -35,3 +49,51 @@ def evaluate_model_print_metrics(true_labels, predicted_labels):
     FPR = FP / (FP + TN)
     print(f"True Positive Rate (TPR/Recall): {TPR:.2f}")
     print(f"False Positive Rate (FPR): {FPR:.2f}")
+
+def calc_roc_curves(true_labels, predicted_scores, run_folder_path):
+    """
+    Plots ROC curves in both linear and logarithmic scales using RocCurveDisplay and returns the figure objects.
+    It also plots the threshold points that make up the ROC curve.
+
+    Args:
+    true_labels (array-like): True binary labels.
+    predicted_scores (array-like): Predicted scores or probabilities for the positive class.
+    
+    Returns:
+    fig_linear, fig_log: Figures for the linear and logarithmic ROC curves with threshold points.
+    """
+
+    print("\nCalculating ROC curves on test set...")
+    
+    debug_run_folder_path = os.path.join(run_folder_path, "debug")
+
+    # Calculate ROC curve and AUC
+    fpr, tpr, thresholds = roc_curve(true_labels, predicted_scores)
+    roc_auc = auc(fpr, tpr)
+    
+    # Linear scale plot
+    fig_linear, ax_linear = plt.subplots()
+    RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc).plot(ax=ax_linear)
+    # Plot the threshold points for the linear scale
+    ax_linear.scatter(fpr, tpr, color='red', s=10, zorder=2)
+    ax_linear.set_title('Receiver Operating Characteristic (Linear Scale)')
+
+    # Logarithmic scale plot
+    fig_log, ax_log = plt.subplots()
+    # Set the minimum value for fpr to avoid log(0) and set lower bound for log scale to 10^-5
+    fpr_log = np.clip(fpr, a_min=10**-5, a_max=None)
+    RocCurveDisplay(fpr=fpr_log, tpr=tpr, roc_auc=roc_auc).plot(ax=ax_log)
+    # Plot the threshold points for the logarithmic scale
+    ax_log.scatter(fpr_log, tpr, color='red', s=10, zorder=2)
+    ax_log.set_xscale('log')
+    # Set limits for the x-axis to start from 10^-5
+    ax_log.set_xlim(left=10**-5)
+    ax_log.set_title('Receiver Operating Characteristic (Logarithmic Scale)')
+
+    # Debug 
+    save_array_to_file(array=np.column_stack((fpr, tpr, thresholds)), 
+                       file_name="fpr_tpr_threshold", 
+                       save_path=debug_run_folder_path)
+
+    # Return the figure objects
+    return fig_linear, fig_log
